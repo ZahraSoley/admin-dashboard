39493eff7e5981859cd548260d38e7293b237dce
ZahraSoley <zahra.soleymani.dev@gmail.com>
2026-03-10 18:10:40 +0100

    styling the command dialog

--- src/components/Sidebar/CoomandMenu.tsx
+++ src/components/Sidebar/CoomandMenu.tsx
@@ -22,20 +22,33 @@ const CommandMenu = ({ open, setOpen }: propsType) => {
     }, [])
 
     return (
-        <Command.Dialog open={open} onOpenChange={setOpen} label="Global Command Menu">
-            <Command.Input />
-            <Command.List>
-                <Command.Empty>No results found.</Command.Empty>
+        <Command.Dialog
+            open={open}
+            onOpenChange={setOpen}
+            label="Global Command Menu"
+            className='fixed inset-0 bg-stone-950/50'
+            onClick={() => setOpen(false)}
+        >
+            <div onClick={(e) => e.stopPropagation()}
+                className='bg-white rounded-lg shadow-xl border-stone-300 border overflow-hidden w-full max-w-lg mx-auto mt-12'
+            >
+                <Command.Input 
+                placeholder='what do you need to do?'
+                className='focus:outline-1 focus:outline-stone-300 w-full border-b border-stone-300 px-3 py-1.5 text-lg placeholder:text-stone-400 relative'
+                />
+                <Command.List>
+                    <Command.Empty>No results found.</Command.Empty>
 
-                <Command.Group heading="Letters">
-                    <Command.Item>a</Command.Item>
-                    <Command.Item>b</Command.Item>
-                    <Command.Separator />
-                    <Command.Item>c</Command.Item>
-                </Command.Group>
+                    <Command.Group heading="Letters">
+                        <Command.Item>a</Command.Item>
+                        <Command.Item>b</Command.Item>
+                        <Command.Separator />
+                        <Command.Item>c</Command.Item>
+                    </Command.Group>
 
-                <Command.Item>Apple</Command.Item>
-            </Command.List>
+                    <Command.Item>Apple</Command.Item>
+                </Command.List>
+            </div>
         </Command.Dialog>
     )
 }
