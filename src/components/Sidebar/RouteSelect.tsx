f49400d538dbaf6d78c11699c89b0707d57e92eb
ZahraSoley <zahra.soleymani.dev@gmail.com>
2026-03-12 16:51:47 +0100

    finished the initial look of RouteSelect

--- src/components/Sidebar/RouteSelect.tsx
+++ src/components/Sidebar/RouteSelect.tsx
@@ -1,18 +1,17 @@
-import { Home, type LucideIcon } from "lucide-react";
+import { DollarSign, Home, Link, Paperclip, User, type LucideIcon } from "lucide-react";
 
-const buttons = [
-    {
-        Icon: <Home />,
-        title: 'Dashboard',
-
-    }
-]
-
+// const buttons = []
 
 
 const RouteSelect = () => {
     return (
-        <div className="space-y-1"></div>
+        <div className="space-y-1">
+            <Route Icon={Home} selected={true} title="Dashboard" />
+            <Route Icon={User} selected={false} title="Team" />
+            <Route Icon={Paperclip} selected={false} title="Invoices" />
+            <Route Icon={Link} selected={false} title="Integration" />
+            <Route Icon={DollarSign} selected={false} title="Finance" />
+        </div>
     )
 }
 export default RouteSelect
@@ -20,14 +19,14 @@ export default RouteSelect
 const Route = ({ selected, Icon, title }: { selected: boolean; Icon: LucideIcon; title: string }) => {
     return (
         <button
-            className={`flex items-center justify-start gap-2 w-full rounded px-2 py-1.5 text-sm transition-colors transition-shadow
+            className={`flex items-center justify-start gap-2 w-full rounded px-2 py-1.5 text-sm transition-colors cursor-pointer
                 ${selected
-                    ? 'bg-white text-stone-950 shadow'
-                    : ':hover:bg-stone-200 bg-transparent text-stone-500 shadow-none'
+                    ?'bg-white text-stone-950 shadow'
+                    :'hover:bg-stone-200 bg-transparent text-stone-500 shadow-none'
                 }
                 `}
         >
-            <Icon className={selected ? 'text-sky-600' : ''} />
+            <Icon className={`size-4 ${selected ? 'text-sky-600' : ''}`} />
             <span>{title}</span>
         </button>
     )
